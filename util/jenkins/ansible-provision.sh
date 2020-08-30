1e1f3d336da19dd4d5e639ffef7bfa0490c50b2f
nadeemshahzad <nshahzad@edx.org>
2020-08-30 21:35:29 +0500

    fix

--- util/jenkins/ansible-provision.sh
+++ util/jenkins/ansible-provision.sh
@@ -476,7 +476,7 @@ EOF
 fi
 
 declare -A deploy
-plays="prospectus edxapp forum ecommerce credentials discovery analyticsapi veda_web_frontend veda_pipeline_worker veda_encode_worker video_pipeline_integration notifier xqueue certs demo testcourses registrar program_manager learner_portal"
+plays="prospectus  ecommerce credentials discovery analyticsapi veda_web_frontend veda_pipeline_worker veda_encode_worker video_pipeline_integration notifier xqueue certs demo testcourses registrar program_manager learner_portal"
 
 for play in $plays; do
     deploy[$play]=${!play}
